8882d10d0e649627f294841abaec0d2ff55bc705
OlegKovalik <oleg.kovalik@gmail.com>
2018-03-12 22:15:35 +0200

    First commit

---
+++ src/main/java/com/jujatask/exception/UserAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.jujatask.exception;
+
+/**
+ * Created by Oleg on 12.03.2018.
+ */
+public class UserAlreadyExistException {
+}
---
+++ src/main/java/com/jujatask/pojo/Response.java
@@ -0,0 +1,7 @@
+package com.jujatask.pojo;
+
+/**
+ * Created by Oleg on 12.03.2018.
+ */
+public class Response {
+}
